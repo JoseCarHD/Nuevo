85c0a9d797753f266afdcde376c7a7ac2a15779c
JoseCarHD <carlos1duart@gmail.com>
2023-08-28 22:52:08 -0600

    Se creo el archivo tarea

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hola!")
+let a = 2, b = 5, r, s = "Exponente = "
+r = a ** b 
+
+const persona = {
+    nombre: "Jose Carlos",
+    apellidoPaterno: "Herrera",
+    apellidoMaterno: "Duuarte",
+    semestre: 7
+}
+
+const fruta= ["naranja", "manzana", "pera", "uva", "platano", true, 5, 7, persona, new Date()]
+ 
+
+// console.log(fruta[9])
+
+function promedio(u1,u2,u3,u4){
+    let prom = (u1 + u2 + u3 + u4)/ 4 
+    return prom 
+}
+
+
+
+console.log(promedio(8.4,9.7,7.5,9.2))
