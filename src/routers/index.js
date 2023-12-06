2b3f7c1774f2def266b8d31a06d7599c5c177867
JoseCarHD <carlos1duart@gmail.com>
2023-12-05 22:50:10 -0600

    Se agrego una mejor interfaz y se completo el sistema CRUD

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,35 +1,58 @@
 const express = require('express');
 const router = express.Router();
-const mode1 = require("../models/datos")();
 const Paciente = require("../models/datos");
 
-router.get('/', async (req,res) =>{
-    const datos = await Paciente.find();
-    console.log("Los datos son: ".datos);
-    res.render('index.ejs',{
-        datos
-    });
+router.get('/', async (req, res) => {
+    try {
+        const datos = await Paciente.find();
+        console.log("Los datos son: ", datos);
+        res.render('index.ejs', {
+            datos
+        });
+    } catch (error) {
+        console.error("Error al obtener datos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
 });
 
-router.post('/add', async (req, res) =>{
-    const expediente = new Paciente(req.body);
-    console.log(req.body);
-    await expediente.save();
-    res.redirect('/');
+router.post('/add', async (req, res) => {
+    try {
+        const expediente = new Paciente(req.body);
+        console.log(req.body);
+        await expediente.save();
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error al agregar datos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
 });
 
-router.get('/del/:id', async(req,res)=>{
-    const {id}= req.params;
-    await Paciente.findByIdAndRemove(id);
-    res.redirect('/');
+router.get('/del/:id', async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params;
+        await Paciente.findByIdAndRemove(id);
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error al eliminar datos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
 });
 
 router.post('/upd/:id', async (req, res) => {
-    const { id } = req.params;
-    const datos = req.body;
+    try {
+        const {
+            id
+        } = req.params;
+        const datos = req.body;
 
-    await Paciente.findByIdAndUpdate(id, producto);
-    res.redirect('/');
+        await Paciente.findByIdAndUpdate(id, datos);
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error al actualizar datos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
 });
 
 module.exports = router;
